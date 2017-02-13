d32c209199755a2fd6d70ebe55c3623113c3b2dd
praveensingh <praveen@Praveens-MacBook-Pro.local>
2017-02-12 23:06:23 -0500

    updated filees

--- src/main/java/com/findmefood/googleplace/ClientKey.java
+++ src/main/java/com/findmefood/googleplace/ClientKey.java
@@ -1,12 +1,12 @@
 package com.findmefood.googleplace;
 
 import org.springframework.stereotype.Component;
-import se.walkercrou.places.GooglePlaces;
+//import se.walkercrou.places.GooglePlaces;
 
 /**
  * Created by praveen on 12/02/17.
  */
 @Component
 public class ClientKey {
-     GooglePlaces client = new GooglePlaces("AIzaSyAGHTEDVrk4tz7QSEQnC62uMyDhORz0Hjs");
+//     GooglePlaces client = new GooglePlaces("AIzaSyAGHTEDVrk4tz7QSEQnC62uMyDhORz0Hjs");
 }
--- src/main/java/com/findmefood/googleplace/GooglePlaceFinder.java
+++ src/main/java/com/findmefood/googleplace/GooglePlaceFinder.java
@@ -4,9 +4,9 @@ package com.findmefood.googleplace;
 import com.findmefood.model.RestaurantDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import se.walkercrou.places.GooglePlaces;
-import se.walkercrou.places.Param;
-import se.walkercrou.places.Place;
+//import se.walkercrou.places.GooglePlaces;
+//import se.walkercrou.places.Param;
+//import se.walkercrou.places.Place;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,24 +21,24 @@ public class GooglePlaceFinder {
         this.clientKey = clientKey;
     }
 
-    private List<Place> places;
-
-    public List<RestaurantDetails> getResults(String query, String location) {
-        places = new ArrayList<>();
-        places = clientKey.client.getPlacesByQuery(query, GooglePlaces.MAXIMUM_PAGE_RESULTS,
-                Param.name("language").value("en"),
-                Param.name("opennow").value(false),
-                Param.name("type").value("restaurant"),
-                Param.name("location").value(location),
-                Param.name("radius").value("1000"));
-//                Param.name("rankby").value("distance"));
-        List<RestaurantDetails> lis = new ArrayList<>();
-        for (int i=0;i< (3 < places.size() ? 3 : places.size());i++) {
-            lis.add(new RestaurantDetails(places.get(i).getName(),
-                                            places.get(i).getAddress(),
-                                            places.get(i).getPhoneNumber()));
-        }
-        return lis;
-    }
+//    private List<Place> places;
+//
+//    public List<RestaurantDetails> getResults(String query, String location) {
+//        places = new ArrayList<>();
+//        places = clientKey.client.getPlacesByQuery(query, GooglePlaces.MAXIMUM_PAGE_RESULTS,
+//                Param.name("language").value("en"),
+//                Param.name("opennow").value(false),
+//                Param.name("type").value("restaurant"),
+//                Param.name("location").value(location),
+//                Param.name("radius").value("1000"));
+////                Param.name("rankby").value("distance"));
+//        List<RestaurantDetails> lis = new ArrayList<>();
+//        for (int i=0;i< (3 < places.size() ? 3 : places.size());i++) {
+//            lis.add(new RestaurantDetails(places.get(i).getName(),
+//                                            places.get(i).getAddress(),
+//                                            places.get(i).getPhoneNumber()));
+//        }
+//        return lis;
+//    }
 
 }
